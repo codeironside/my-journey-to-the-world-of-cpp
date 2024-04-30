7a89848503d480a539c264ba51dd7adb0e3f82f7
oluwatobi ayoola jolaosho <fury25423@gmail.com>
2024-04-30 19:06:48 +0100

    tried out two more tasks

--- testone/test.cpp
+++ testone/test.cpp
@@ -1,6 +1,25 @@
-#include <iostream>
+/* #include <iostream>
 using std::cout;
 int main(){
 cout <<"Hello world";
+return 0;
+}
+*?
+
+#include <iostream>
+
+using namespace std;
+int main(){
+double m = 2m;
+cout << mate;
+return 0;
+}
+*/
+#include <iostream>
+using std::cout;
+int main(){
+int i= 2 ;
+float j= i^31;
+cout << j << "\n";
 return 0;
 }
